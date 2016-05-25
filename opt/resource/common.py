74553b6cedf4b8d682cd250f81540eff8567f7bf
Martin Domke <mdomke@jackbird.local>
2016-05-25 13:31:23 +0200

    Add logging of working dir and payload

--- opt/resource/common.py
+++ opt/resource/common.py
@@ -1,10 +1,16 @@
 import json
+import tempfile
 from urllib.parse import urljoin
 import sys
 
 
 def get_payload():
-    return json.load(sys.stdin)
+    payload = json.load(sys.stdin)
+    _, fname = tempfile.mkstemp()
+    print("Logging payload to {}".format(fname), file=sys.stderr)
+    with open(fname, 'w') as fp:
+        fp.write(json.dumps(payload))
+    return payload
 
 
 def get_index_url(payload):
