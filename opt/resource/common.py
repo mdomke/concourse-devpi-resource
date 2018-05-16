f59082265833602832668a90ebdace04e39d68b5
Martin Domke <mail@martindomke.net>
2018-05-16 10:14:10 +0200

    Use format strings where suitable

--- opt/resource/common.py
+++ opt/resource/common.py
@@ -8,7 +8,7 @@ import sys
 def get_payload():
     payload = json.load(sys.stdin)
     _, fname = tempfile.mkstemp()
-    print("Logging payload to {}".format(fname), file=sys.stderr)
+    print(f"Logging payload to {fname}", file=sys.stderr)
     with open(fname, 'w') as fp:
         fp.write(json.dumps(payload))
     return payload
